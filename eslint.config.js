4ce5892995d05b41bea45b3abbbdfb59aa93fd2b
iman malekian <imanmalekian31@gmail.com>
2024-11-21 07:21:13 +0330

    init

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import pluginJs from '@eslint/js';
+import pluginVue from 'eslint-plugin-vue';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,ts,vue}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  ...pluginVue.configs['flat/recommended'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+  },
+  {
+    rules: {
+      'vue/max-attributes-per-line': 'off',
+    },
+  },
+];
